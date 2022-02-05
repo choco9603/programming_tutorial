bca01843c2f91e809b8ef9fccfa5d821ff3090fb
choco9603 <309choco9603@gmail.com>
2022-02-05 17:16:31 +0900

    test

---
+++ TypeScript_tutorial/log.ts
@@ -0,0 +1,15 @@
+// export function primitiveSample(){
+//   let name: string = 'kumoko';
+//   console.log("primitive sample 1:",typeof name, name);
+  
+//   let age: number = 16;
+//   console.log("primitive sample 2:",typeof age, age);
+  
+//   let anime: boolean = true;
+//   console.log("primitive sample 3:",typeof anime, anime);
+  
+//   const isOver15: boolean = age >= 15;
+//   console.log("primitive sample 4:",typeof isOver15, isOver15);
+// }
+
+export {default as primitiveSample} from './basic/primitive';
---
+++ TypeScript_tutorial/main.ts
@@ -0,0 +1,4 @@
+import { primitiveSample } from './log';
+primitiveSample();
+
+
